e896c91574a1da76b6b050e9f99adaa871c2ec9f
lulang <lulang@zhiannet.com>
2021-06-08 11:14:53 +0800

    hids 主机防护组件api接口对接 const

---
+++ hids/const/const.go
@@ -0,0 +1,10 @@
+package _const
+
+const (
+	Org_api_url  = "passport/thirdApi/createOrg"
+	Sign_method  = "HmacSHA256"
+	Sign_version = "3.0"
+	Version      = "3.0"
+	Appid        = "39rkz"
+	Secret       = "tkvgpvjuht2625mo"
+)
