29d3877721f7bb9079c353f2e06d5f35beb49676
lulang <lulang@zhiannet.com>
2021-06-15 20:26:27 +0800

    awvs 漏洞扫描组件api接口对接 reports server 报表生成删除下载

---
+++ awvs/server/reports/reports.go
@@ -0,0 +1,22 @@
+package reports
+
+import (
+	"github.com/1uLang/zhiannet-api/awvs/model/reports"
+)
+
+//scans 扫描接口层
+
+//List 扫描列表
+func List(req *reports.ListReq) (info map[string]interface{}, err error) {
+	return reports.List(req)
+}
+
+//Create 新建扫描
+func Create(req *reports.CreateResp) (info map[string]interface{}, err error) {
+	return reports.Create(req)
+}
+
+//Delete 删除扫描
+func Delete(scan_id string) (err error) {
+	return reports.Delete(scan_id)
+}
